5463043910b0dc797f8d83844326ca106d611739
유도영 <dbehdud35@naver.com>
2023-02-27 22:05:46 +0900

    0223_1

---
+++ myapp/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Bootstrap
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <div>hello!!</div>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
