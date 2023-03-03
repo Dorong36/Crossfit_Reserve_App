84aa0698df873781ca9f88204f451fc00f4d026e
유도영 <dbehdud35@naver.com>
2023-03-03 15:16:25 +0900

    0303

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 // Bootstrap
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <div>hello!!</div>
-      <Button variant="primary">Primary</Button>
+      <div className='container'>
+        <div>hello!!</div>
+        <Card className="text-center">
+          <Card.Header>Work Of Day</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
+      </div>
     </div>
   );
 }
